aeba1bb00a427df745589a2541887bbf2e95c851
Marcelo Siqueira Filho <marcelosiqqueira@gmail.com>
2024-01-26 23:53:42 -0300

    feat: created front login page

---
+++ frontend/best-food-app/src/app/pages/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor( private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    console.log('Formulário enviado:', this.loginForm.value);
+  }
+}
