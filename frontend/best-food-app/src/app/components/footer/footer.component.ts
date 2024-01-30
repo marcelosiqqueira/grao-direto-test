304f2972add6f7c99a9f4529c67a3fd566026df1
Marcelo Siqueira Filho <marcelosiqqueira@gmail.com>
2024-01-30 15:14:45 -0300

    refact: changes header layout

--- frontend/best-food-app/src/app/components/footer/footer.component.ts
+++ frontend/best-food-app/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  constructor(
+    private readonly router: Router
+  ) {}
+
+  logoff() {
+    localStorage.clear();
+    this.navigateToLogin();
+  }
+
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
 }
